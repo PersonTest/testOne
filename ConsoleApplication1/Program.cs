29807eda8b8ded6efc9c33691464bedbc6e2eeb0
wpfshine <wangpengfei@shinetechchina.com>
2016-05-03 16:10:50 +0800

    init

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            animal a = new animal();
+            animal an = new cat();
+            cat c = new cat();
+            a.caneat();
+            a.canlive();
+            a.cansleep();
+            Console.WriteLine("........................");
+            an.caneat();
+            an.canlive();
+            an.cansleep();
+            Console.WriteLine("........................");
+            c.caneat();
+            c.canlive();
+            c.cansleep();
+            Console.WriteLine("........................");
+            Console.ReadKey();
+        }
+    }
+    public abstract class Biology
+    {
+        public virtual void canlive()
+        {
+            Console.WriteLine("can live");
+        }
+    }
+    public class animal : Biology
+    {
+        public override void canlive()
+        {
+            Console.WriteLine("animal can live");
+        }
+
+        public void cansleep()
+        {
+            Console.WriteLine("animal can sleep");
+        }
+        public virtual void caneat()
+        {
+            Console.WriteLine("animal can eat");
+        }
+    }
+    public class cat : animal
+    {
+        public override void caneat()
+        {
+            //base.caneat();
+            Console.WriteLine("cat can eat");
+        }
+
+        public new void cansleep()
+        {
+            Console.WriteLine("cat can sleep");
+        }
+    }
+}
